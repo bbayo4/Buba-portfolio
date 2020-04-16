10421ae502a086e44cc26c43c44c71db7c605e5c
Buba <buba.bayo@siriusxm.com>
2020-04-15 23:18:58 -0400

    add the build file

---
+++ client/build/precache-manifest.8bd995286ee4d20d9a386cc191062a85.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7644cca518470776c37de8485de984bb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b06510dd9dc43be7b854",
+    "url": "/static/css/main.e797cc37.chunk.css"
+  },
+  {
+    "revision": "5c3f9a19deffe40afe42",
+    "url": "/static/js/2.9a62958a.chunk.js"
+  },
+  {
+    "revision": "890b83c3b503af9a5630afd9ff112890",
+    "url": "/static/js/2.9a62958a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b06510dd9dc43be7b854",
+    "url": "/static/js/main.d9b1d65a.chunk.js"
+  },
+  {
+    "revision": "0098a8c0da1dcb141fbf",
+    "url": "/static/js/runtime-main.8022c083.js"
+  }
+]);
